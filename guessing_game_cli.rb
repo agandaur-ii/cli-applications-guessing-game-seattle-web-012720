106d9056688ff2b2a3a2eff014d9e6b23cd2b010
Christopher Wagstaffe <cjwagstaffe@gmail.com>
2020-01-23 00:07:26 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,8 +1,13 @@
 def pick_a_number
+  puts "Pick a random number between 1 and 6"
   get.chomp
 end
 
 def run_guessing_game
   cp_rand_number = rand(2...5)
   my_rand_number = pick_a_number
+  
+  if cp_rand_number == my_rand_number
+    return 
+  end
 end
