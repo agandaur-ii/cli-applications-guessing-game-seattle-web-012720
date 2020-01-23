4296173fab6fff295182e9f48af72c332fc7500d
Christopher Wagstaffe <cjwagstaffe@gmail.com>
2020-01-23 00:17:44 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -7,6 +7,10 @@ def run_guessing_game
   cp_rand_number = rand(6) + 1
   my_rand_number = pick_a_number
   
+  if my_rand_number == "exit"
+    return "Goodbye!"
+  end
+  
   if cp_rand_number == my_rand_number
     return "You guessed the correct number!"
   end
@@ -15,9 +19,7 @@ def run_guessing_game
     return "Sorry! The computer guessed #{cp_rand_number}."
   end
   
-  if my_rand_number == "exit"
-    return "Goodbye!"
-  end
+
 end
 
 pp run_guessing_game
